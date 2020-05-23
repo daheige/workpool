7afc51f7ed828a9b97bf29c73fa716c0c71c4dff
daheige <zhuwei313@hotmail.com>
2020-05-23 20:13:27 +0800

    workpool graceful exit

--- pool.go
+++ pool.go
@@ -24,6 +24,10 @@ func NewTask(fn func() error) *Task {
 
 // run exec a task.
 func (t *Task) run(logEntry logger) {
+	if t == nil {
+		return
+	}
+
 	defer func() {
 		if e := recover(); e != nil {
 			logEntry.Println("exec current task panic: ", e)
@@ -40,26 +44,42 @@ type logger interface {
 
 // Pool task work pool
 type Pool struct {
-	EntryChan chan *Task
-	jobChan   chan *Task
-	workerNum int            // worker num
-	logEntry  logger         // logger interface
-	interrupt chan os.Signal // exit signal,ctrl +c to interrupt work pool
-	stop      chan bool      // shutdown stop signal
-	wait      time.Duration  // graceful shutdown to wait time
+	entryChan    chan *Task     // task entry chan
+	jobChan      chan *Task     // job chan
+	workerNum    int            // worker num
+	logEntry     logger         // logger interface
+	stop         chan struct{}  // stop sem
+	interrupt    chan os.Signal // interrupt signal
+	wait         time.Duration  // close entry chan wait time,default 5s
+	shutdownWait time.Duration  // work pool shutdown wait time,default 3s
 }
 
-// defaultMaxEntryCap default max entry chan num.
-var defaultMaxEntryCap = 10000
+var (
+	// defaultMaxEntryCap default max entry chan num.
+	defaultMaxEntryCap = 10000
 
-// defaultMaxJobCap default max job chan cap.
-var defaultMaxJobCap = 10000
+	// defaultMaxJobCap default max job chan cap.
+	defaultMaxJobCap = 10000
+
+	// defaultMaxWorker default max worker num
+	// the number of workers depends on the specific business.
+	defaultMaxWorker = 100
+
+	est = struct{}{}
+
+	// LogDebug log task run in which worker.
+	LogDebug = false
+)
 
 // Option func Option to change pool
 type Option func(p *Pool)
 
 // NewPool returns a pool.
 func NewPool(num int, ec int, jc ...int) *Pool {
+	if num < 0 || num >= defaultMaxWorker {
+		num = defaultMaxWorker
+	}
+
 	if ec < 0 || ec >= defaultMaxEntryCap {
 		ec = defaultMaxEntryCap
 	}
@@ -75,10 +95,11 @@ func NewPool(num int, ec int, jc ...int) *Pool {
 	}
 
 	p := &Pool{
-		workerNum: num,
-		stop:      make(chan bool, 1),
-		interrupt: make(chan os.Signal, 1),
-		wait:      5 * time.Second,
+		workerNum:    num,
+		stop:         make(chan struct{}, 1),
+		wait:         5 * time.Second,
+		shutdownWait: 3 * time.Second,
+		interrupt:    make(chan os.Signal, 1),
 	}
 
 	if jobChanCap == 0 {
@@ -89,10 +110,10 @@ func NewPool(num int, ec int, jc ...int) *Pool {
 	}
 
 	if ec == 0 {
-		// no buf for EntryChan.
-		p.EntryChan = make(chan *Task)
+		// no buf for entryChan.
+		p.entryChan = make(chan *Task)
 	} else {
-		p.EntryChan = make(chan *Task, ec)
+		p.entryChan = make(chan *Task, ec)
 	}
 
 	if p.logEntry == nil {
@@ -109,106 +130,138 @@ func WithLogger(logEntry logger) Option {
 	}
 }
 
-// WithWaitTime task pool shutdown to wait time.
+// WithWaitTime close entry chan wait time.
 func WithWaitTime(d time.Duration) Option {
 	return func(p *Pool) {
 		p.wait = d
 	}
 }
 
-// AddTask add a task to p.EntryChan.
-func (p *Pool) AddTask(t *Task) {
-	select {
-	case <-p.stop:
-	default:
-		p.EntryChan <- t
+// WithWorkerNum change worker num.
+func WithWorkerNum(num int) Option {
+	return func(p *Pool) {
+		p.workerNum = num
 	}
 }
 
-// BatchAddTask batch add task to p.EntryChan.
+// WithShutdownWait change shutdown wait time.
+func WithShutdownWait(d time.Duration) Option {
+	return func(p *Pool) {
+		p.shutdownWait = d
+	}
+}
+
+// AddTask add a task to p.entryChan.
+func (p *Pool) AddTask(t *Task) {
+	if t == nil {
+		return
+	}
+
+	defer p.recovery()
+
+	select {
+	case <-p.stop:
+		return
+	default:
+		p.entryChan <- t
+	}
+}
+
+// BatchAddTask batch add task to p.entryChan.
 func (p *Pool) BatchAddTask(t []*Task) {
+	defer p.recovery()
+
 	for k := range t {
+		if t[k] == nil {
+			continue
+		}
+
 		select {
 		case <-p.stop:
 			return
 		default:
-			p.EntryChan <- t[k]
+			p.entryChan <- t[k]
 		}
 	}
 }
 
 // exec exec task from job chan.
-func (p *Pool) exec(id int) {
+func (p *Pool) exec(id int, done chan struct{}) {
 	defer p.recovery()
 
 	defer func() {
+		done <- est
 		p.logEntry.Println("current worker id: ", id, "will exit...")
 	}()
 
 	// get task from JobChan to run.
 	for task := range p.jobChan {
 		task.run(p.logEntry)
-		p.logEntry.Println("current worker id: ", id)
+		if LogDebug {
+			p.logEntry.Println("current worker id: ", id)
+		}
 	}
 }
 
 // Run create workerNum goroutine to exec task.
 func (p *Pool) Run() {
 	p.logEntry.Println("exec task begin...")
-	signal.Notify(p.interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR2, os.Interrupt, syscall.SIGHUP)
+	signal.Notify(p.interrupt, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGHUP)
+
+	doneBuf := make(chan struct{}, p.workerNum)
 
 	// create p.workerNum goroutine to do task
 	for i := 0; i < p.workerNum; i++ {
-		go p.exec(i)
+		go p.exec(i+1, doneBuf)
 	}
 
-	// listen interrupt signal.
+	// throw entry chan task to JobChan
 	go func() {
-		for {
-			if p.isInterrupt() {
-				p.stop <- true
+		defer close(p.jobChan)
 
-				p.logEntry.Println("task will exit...")
-
-				// Create a deadline to wait for all task graceful exit.
-				ctx, cancel := context.WithTimeout(context.Background(), p.wait)
-				defer cancel()
-
-				// Doesn't block if no task, but will otherwise wait
-				// until the timeout deadline.
-				// Optionally, you could run shutdown in a goroutine and block on
-				// if your application should wait for all task to run
-				// to finalize based on context cancellation.
-				<-ctx.Done()
-
-				// close entry chan and job chan to graceful shutdown.
-				close(p.EntryChan)
-				close(p.jobChan)
-
-				break
-			}
+		// If the entry channel is closed, the block will be lifted until consumption is completed.
+		for task := range p.entryChan {
+			p.jobChan <- task
 		}
 	}()
 
-	// throw task to JobChan
-	for {
-		select {
-		case p.jobChan <- <-p.EntryChan:
-		case <-p.stop:
-			return
-		}
+	// listen interrupt signal for work pool graceful exit.
+	go func() {
+		// Block until we receive stop signal.
+		sig := <-p.interrupt
+		p.logEntry.Println("recv signal: ", sig.String())
+		close(p.stop)
+
+		// Here you need to wait for the task that has been sent to the entry chan
+		// to ensure that it can be sent successfully
+		ctx, cancel := context.WithTimeout(context.Background(), p.wait)
+		defer cancel()
+
+		<-ctx.Done()
+
+		close(p.entryChan)
+	}()
+
+	// wait all job chan task to finish.
+	for i := 0; i < p.workerNum; i++ {
+		<-doneBuf
 	}
+
+	p.logEntry.Println("work pool shutdown success")
 }
 
-// isInterrupt return true,if receive a signal to interrupt task exec.
-func (p *Pool) isInterrupt() bool {
-	select {
-	case sg := <-p.interrupt:
-		p.logEntry.Println("received signal: ", sg.String())
-		return true
-	default:
-		return false
-	}
+// Shutdown If all task are sent to the task entry chan, you can call this method to exit smoothly.
+func (p *Pool) Shutdown() {
+	// Create a deadline to manual exit wait time.
+	ctx, cancel := context.WithTimeout(context.Background(), p.wait)
+	defer cancel()
+
+	// Doesn't block if no task, but will otherwise wait
+	// until the timeout deadline.
+	<-ctx.Done()
+
+	p.interrupt <- syscall.SIGTERM
+	p.logEntry.Println("work pool will shutdown...")
 }
 
 // recovery catch a recover.
